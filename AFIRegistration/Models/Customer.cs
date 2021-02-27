d23c54e026a676ad22bfa8407a719bcb33afa49a
Barrie Cooper <barrie@nouver.net>
2021-02-27 10:39:53 +0000

    Customer model & db context

---
+++ AFIRegistration/Models/Customer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AFIRegistration.Models
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string Surname { get; set; }
+
+        public string PolicyNumber { get; set; }
+
+        public DateTime? DateOfBirth { get; set; }
+
+        public string EmailAddress { get; set; }
+
+        public DateTime Created { get; set; }
+    }
+}
